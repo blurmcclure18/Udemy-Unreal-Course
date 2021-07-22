70200d309b9b03419612cef25d69b3c8c5953b46
blurmcclure18 <blurmcclure16@gmail.com>
2021-07-22 14:16:43 -0500

    Variables and Math

--- C++ TripleX/TripleX.cpp
+++ C++ TripleX/TripleX.cpp
@@ -3,6 +3,17 @@
 int main()
 {
     std::cout << "You need to Kill the Arcane Wizard in his tower...\n";
-    std::cout << "Crack these Mysterious Runic Puzzles to get to him... ";
+    std::cout << "Crack these Mysterious Runic Puzzles to get to him... \n";
+
+    int a = 4;
+    int b = 3;
+    int c = 6;
+
+    int sum = a + b + c;
+    int product = a * b * c;
+
+    std::cout << sum << std::endl;
+    std::cout << product << std::endl;
+
     return 0;
 }
