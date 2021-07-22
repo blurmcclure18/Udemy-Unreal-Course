4a842e6d28598faa59e135e37cef5b196f708fe3
blurmcclure18 <blurmcclure16@gmail.com>
2021-07-22 15:13:20 -0500

    Getting User Input

--- C++ TripleX/TripleX.cpp
+++ C++ TripleX/TripleX.cpp
@@ -3,20 +3,29 @@
 int main()
 {
     //This prints out the first message the player sees
-    std::cout << "You need to Kill the Arcane Wizard in his tower...\n";
-    std::cout << "Crack these Mysterious Runic Puzzles to get to him... \n";
+    std::cout << "\n" << "You need to Kill the Arcane Wizard in his tower...\n";
+    std::cout << "Crack these Mysterious Runic Puzzles to get to him... \n"<< "\n";
 
     //Declare 3 number code
-    const int a = 4;
-    const int b = 3;
-    const int c = 2;
- 
-    const int sum = a + b + c;
-    const int product = a * b * c;
+    const int RuneA = 4;
+    const int RuneB = 3;
+    const int RuneC = 2;
+    
+    const int RuneSum = RuneA + RuneB + RuneC;
+    const int RuneProduct = RuneA * RuneB * RuneC;
 
     //Print sum and product to the terminal
-    std::cout << sum << "\n";
-    std::cout << product << "\n";
+    std::cout << "+ There are 3 numbers in the code. \n";
+    std::cout << "+ The numbers add-up to: " << RuneSum << "\n";
+    std::cout << "+ The numbers multiply to: " << RuneProduct << "\n";
 
+    int GuessA, GuessB, GuessC, GuessSum, GuessProduct;
+    std::cin >> GuessA;
+    std::cin >> GuessB;
+    std::cin >> GuessC;
+
+    GuessSum = GuessA + GuessB + GuessC;
+    GuessProduct = GuessA * GuessB * GuessC;
+    
     return 0;
 }
