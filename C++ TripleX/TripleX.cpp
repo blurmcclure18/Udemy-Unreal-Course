aacac3e37a0f7d65d91aea4efa318d24a42bc954
blurmcclure18 <blurmcclure16@gmail.com>
2021-07-22 15:39:58 -0500

    while loops and functions

--- C++ TripleX/TripleX.cpp
+++ C++ TripleX/TripleX.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 
-int main()
+void PrintIntro()
 {
     //This prints out the first message the player sees
-    std::cout << "\n" << "You need to Kill the Arcane Wizard in his tower...\n";
-    std::cout << "Crack these Mysterious Runic Puzzles to get to him... \n"<< "\n";
+    std::cout << "\n\nYou need to capture the Arcane Wizard at the top of his tower...\n";
+    std::cout << "Unlock the doors in his tower to get to him... \n\n";
+}
+
+void PlayGame()
+{
+    PrintIntro();
 
     //Declare 3 number code
     const int RuneA = 4;
@@ -15,17 +20,35 @@ int main()
     const int RuneProduct = RuneA * RuneB * RuneC;
 
     //Print sum and product to the terminal
-    std::cout << "+ There are 3 numbers in the code. \n";
+    std::cout << "+ There are 3 numbers on the lock. \n";
     std::cout << "+ The numbers add-up to: " << RuneSum << "\n";
     std::cout << "+ The numbers multiply to: " << RuneProduct << "\n";
 
-    int GuessA, GuessB, GuessC, GuessSum, GuessProduct;
-    std::cin >> GuessA;
-    std::cin >> GuessB;
-    std::cin >> GuessC;
+    //Store player guess
+    int GuessA, GuessB, GuessC;
+    std::cin >> GuessA >> GuessB >> GuessC;
 
-    GuessSum = GuessA + GuessB + GuessC;
-    GuessProduct = GuessA * GuessB * GuessC;
-    
+    int GuessSum = GuessA + GuessB + GuessC;
+    int GuessProduct = GuessA * GuessB * GuessC;
+
+    //Check if the players guess is correct
+    if (GuessSum == RuneSum && GuessProduct == RuneProduct)
+    {
+        std::cout << "\nThe door opened! \n";
+    }
+
+    else    
+    {
+        std::cout << "\nThe door remains locked! \n";
+    }
+
+}
+
+int main()
+{
+    while (true)
+    {
+        PlayGame();
+    }
     return 0;
 }
